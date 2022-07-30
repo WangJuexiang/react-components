0cb73cfd92b0f91e5b2c5da5b536e8feafed7117
juexiang.wang <juexiang.wang@thoughtworks.com>
2022-07-30 14:55:51 +0800

    full demo for example1

--- IdeaProjects/react/react-components/my-app/src/App.js
+++ IdeaProjects/react/react-components/my-app/src/App.js
@@ -11,6 +11,22 @@ class App extends React.Component {
     }
   }
 
+  //方法：修改父组件的状态
+  handleClick = () => {
+    this.setState({
+      name: "thougthworks"
+    })
+  }
+
+  //方法：接收子组件传的值,修改state
+
+  cFunc = (e) => {
+    console.log(e)
+    this.setState({
+      txt: "修改后"
+    })
+  }
+
   render() {
     return (
       <div>
@@ -19,7 +35,13 @@ class App extends React.Component {
         <h3>子组件向父组件传值</h3>
         <div>-----分割线-----</div>
 
-        <Children name={this.state.name} ></Children>
+        <Children
+          name={this.state.name}
+          // 通过事件传递的时候，this的指向已经改变——需用bind绑定
+          cFunc={this.cFunc.bind(this)} >
+        </Children>
+        <button onClick={this.handleClick}>按钮</button>
+        <h2>父组件，接收子组件回传的值:{this.state.txt}</h2>
       </div>
 
     )
--- IdeaProjects/react/react-components/my-app/src/component/children.js
+++ IdeaProjects/react/react-components/my-app/src/component/children.js
@@ -2,11 +2,25 @@ import React from "react";
 
 class Children extends React.Component {
 
+	childFunction = (e) => {
+		// console.log(e)
+		// console.log(e.target)
+		// console.log(e.target.value)
+
+		//子组件中调用父组件的方法，并把值传入
+		this.props.cFunc(e.target.value)
+
+
+	}
+
 	render() {
 		return (
 			<div>
 				<h1>子组件接收父组件传值</h1>
 				<h2>{this.props.name}</h2>
+				<h3>子组件向父组件传值</h3>
+				<input type="text" onChange={this.childFunction}></input>
+
 			</div>
 		)
 	}
