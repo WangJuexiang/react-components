1f206e46eca53ccd8ce0a06742c022f66a50c0e9
Juexiang Wang <juexiang.wang@Juexiangs-MacBook-Pro.local>
2022-08-25 16:52:20 +0800

    homepage style

---
+++ IdeaProjects/react/assignment2/my-app/src/components/HomePage/ArticleList/index.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { Button } from 'antd';
+import './index.css'
+
+
+export default class ArticleList extends Component {
+	render() {
+		return (
+			<div className='article-list'>
+				<div className='article-info'>
+					<div className='article-title'>
+						article title (25/8/2022)
+					</div>
+					<div className='article-content'>
+						this is a published article
+					</div>
+				</div>
+				<div className='comment-figure'>
+					评论数:2
+				</div>
+				<div className='article-list-button'>
+					<div className='edit-button'>
+						<Button type="text">edit</Button>
+
+					</div>
+					<div className='delete-button'>
+						<Button type="text">delete</Button>
+
+					</div>
+
+				</div>
+			</div>
+		)
+	}
+}
--- IdeaProjects/react/assignment2/my-app/src/components/HomePage/index.js
+++ IdeaProjects/react/assignment2/my-app/src/components/HomePage/index.js
@@ -1,46 +1,76 @@
 import React, { Component } from 'react';
 import AsideNav from './AsideNav';
+import ArticleList from './ArticleList';
+import { Button } from 'antd';
 import { UserOutlined, CameraOutlined, CloudDownloadOutlined } from '@ant-design/icons';
+import { Input } from 'antd';
 import './index.css'
-
-
-
-
+const { TextArea } = Input;
 
 export default class HomePage extends Component {
 	render() {
 		return (
 			<div>
-				<aside>
-					<div className='homepage'>
-						<UserOutlined style={{ color: "#FFFFFF", fontSize: "150%", marginRight: '20px' }} />
-						<AsideNav content={'主页'} />
-					</div>
-					<div className='hotspot'>
-						<CameraOutlined style={{ color: "#FFFFFF", fontSize: "150%", marginRight: '20px' }} />
-						<AsideNav content={'热点'} />
-					</div>
-					<div className='usercenter'>
-						<CloudDownloadOutlined style={{ color: "#FFFFFF", fontSize: "150%", marginRight: '20px' }} />
-						<AsideNav content={'我的'} />
+				<div className='homepage'>
+					<aside>
+						<div className='homepage'>
+							<UserOutlined style={{ color: "#FFFFFF", fontSize: "150%", marginRight: '20px' }} />
+							<AsideNav content={'主页'} />
+						</div>
+						<div className='hotspot'>
+							<CameraOutlined style={{ color: "#FFFFFF", fontSize: "150%", marginRight: '20px' }} />
+							<AsideNav content={'热点'} />
+						</div>
+						<div className='usercenter'>
+							<CloudDownloadOutlined style={{ color: "#FFFFFF", fontSize: "150%", marginRight: '20px' }} />
+							<AsideNav content={'我的'} />
 
-					</div>
-				</aside>
-				<header>
-					header
-				</header>
-				<main>
-					<section>
-						list
+						</div>
+					</aside>
+
+					<section className='homepage-body'>
+
+						<header>
+
+						</header>
+						<main className='homepage-main'>
+							<section className='list'>
+								<div className='article-list'>
+									<ArticleList />
+								</div>
+
+
+
+							</section>
+							<section className='content'>
+								<div className='title-input'>
+									<Input placeholder="Please input the title" />
+								</div>
+								<div className='content-input'>
+									<TextArea placeholder='What do you want to write...' rows={18} />
+								</div>
+
+								<div className='content-button'>
+									<div className='cancel-button'>
+										<Button>Cancel</Button>
+									</div>
+									<div>
+
+										<Button className='confirm-button' type="primary">Confirm</Button>
+									</div>
+
+
+								</div>
+
+							</section>
+
+						</main>
 
 					</section>
-					<section>
-						content
-					</section>
 
-				</main>
+				</div>
 
-				<footer>footer</footer>
+				{/* <footer>footer</footer> */}
 
 			</div>
 		)
